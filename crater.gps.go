25dee41fda92203e0e4a50affdc48918cce52701
mcthoren <thoren.mcdole@gmail.com>
2021-08-20 18:31:27 +0000

    hook up syncing. note.

--- crater.gps.go
+++ crater.gps.go
@@ -17,6 +17,8 @@ WT_DIR='/import/home/ghz/repos/weather_tools'
 DAT_DIR='/home/ghz/alt/data'
 
 "${WT_DIR}/grab_48h" "${DAT_DIR}" alt.dat
+
+# this could be done once a day
 cat ${DAT_DIR}/*/alt.dat.* | ${GPS_DIR}/alt_hist_gen > "${DAT_DIR}/gps.alt.historgram"
 
 cd /home/ghz/alt/plots || exit 1
@@ -24,6 +26,9 @@ gnuplot "${GPS_DIR}/crater.gps.gnuplot"
 
 sync
 
-# /usr/bin/rsync -ur --timeout=50 /home/ghz/wx /import/home/ghz/repos/dust_wx wx0_sync:/wx0/ 2> /dev/null
+/usr/bin/rsync -ur --timeout=50 /home/ghz/alt/ /import/home/ghz/repos/gps/crater.gps.gnuplot \
+	/import/home/ghz/repos/gps/crater.gps.go /import/home/ghz/repos/gps/alt_hist_gen \
+	/import/home/ghz/repos/gps/py_gps_4 /import/home/ghz/repos/gps/README \
+	wx9_sync:/wx9/ # 2> /dev/null
 
 rm "${LOCK}"
